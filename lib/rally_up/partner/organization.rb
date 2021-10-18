5b5aa74e31820d04c1ac4f2572864e2c60f7ad95
David Richey <daveyrichey@gmail.com>
2021-10-18 11:10:50 -0400

    Custom Organization
    * Add
    * Approve
    * Delete

--- lib/rally_up/partner/organization.rb
+++ lib/rally_up/partner/organization.rb
@@ -6,7 +6,7 @@ module RallyUp
       attr_accessor :id, :ein, :name, :common_name, :email, :url,
                     :is_tax_exempt, :address, :organization_logo_url,
                     :organization_website_url, :organization_facebook_url,
-                    :organization_twitter_url, :primary_currency
+                    :organization_twitter_url, :primary_currency, :status
 
       def initialize(json)
         @id = json[:Id]
@@ -22,9 +22,40 @@ module RallyUp
         @organization_facebook_url = json[:OrganizationFacebookUrl]
         @organization_twitter_url = json[:OrganizationTwitterUrl]
         @primary_currency = json[:PrimaryCurrency]
+        @status = json[:Status]
       end
 
       class << self
+        def approve(id)
+          json = RallyUp::Partner.json(:post, '/v1/partnerapi/approvecustomorg', params: {
+                                         OrganizationID: id
+                                       })
+          new(json[:Result].to_h)
+        end
+
+        def create(attrs = {})
+          json = RallyUp::Partner.json(:post, '/v1/partnerapi/addcustomorg', params: {
+            :Name => attrs[:name],
+            :Country => attrs[:country],
+            :Address1 => attrs[:address1],
+            :Address2 => attrs[:address2],
+            :Address3 => attrs[:address3],
+            :State => attrs[:state],
+            :City => attrs[:city],
+            :PostalCode => attrs[:postal_code],
+            '501c' => attrs['501c'],
+            :EIN => attrs[:ein]
+          }.reject { |_k, v| v.nil? })
+          new(json[:Result].to_h)
+        end
+
+        def delete(id)
+          json = RallyUp::Partner.json(:delete, '/v1/partnerapi/deletecustomorg', params: {
+                                         OrganizationID: id
+                                       })
+          new(json[:Result].to_h)
+        end
+
         def list(start_date: nil, end_date: nil)
           json = RallyUp::Partner.json(:get, '/v1/partnerapi/organizations', params: {
             endDate: end_date,
--- spec/rally_up/partner/organization_spec.rb
+++ spec/rally_up/partner/organization_spec.rb
@@ -9,6 +9,80 @@ RSpec.describe RallyUp::Partner::Organization do
     RallyUp::Partner.token = 'tok3n'
   end
 
+  describe '#approve' do
+    let(:id) { SecureRandom.hex(3) }
+    it 'makes HTTP POST request and renders Organizations' do
+      stub_request(:post, 'https://my.partnerdomain.com/v1/partnerapi/approvecustomorg')
+        .with(
+          headers: { 'Host' => 'my.partnerdomain.com', 'Authorization' => 'Bearer tok3n' },
+          body: "OrganizationID=#{id}"
+        )
+        .to_return(status: 200, body: organization, headers: {})
+
+      organization = RallyUp::Partner::Organization.approve(id)
+      expect(organization.id).to eq(12_345)
+    end
+  end
+
+  describe '#create' do
+    let(:attrs) do
+      {
+        name: 'name',
+        country: 'country',
+        address1: 'address1',
+        address2: 'address2',
+        address3: 'address3',
+        state: 'state',
+        city: 'city',
+        postal_code: 'postalcode',
+        '501c' => '501c',
+        ein: 'ein'
+      }
+    end
+
+    let(:expected) do
+      {
+        Name: 'name',
+        Country: 'country',
+        Address1: 'address1',
+        Address2: 'address2',
+        Address3: 'address3',
+        State: 'state',
+        City: 'city',
+        PostalCode: 'postalcode',
+        '501c' => '501c',
+        EIN: 'ein'
+      }
+    end
+
+    it 'makes HTTP GET request and renders Organizations' do
+      stub_request(:post, 'https://my.partnerdomain.com/v1/partnerapi/addcustomorg')
+        .with(
+          headers: { 'Host' => 'my.partnerdomain.com', 'Authorization' => 'Bearer tok3n' },
+          body: expected.map { |k, v| "#{k}=#{v}" }.join('&')
+        )
+        .to_return(status: 200, body: organization, headers: {})
+
+      organization = RallyUp::Partner::Organization.create(attrs)
+      expect(organization.id).to eq(12_345)
+    end
+  end
+
+  describe '#delete' do
+    let(:id) { SecureRandom.hex(3) }
+    it 'makes HTTP DELETE request and renders Organizations' do
+      stub_request(:delete, 'https://my.partnerdomain.com/v1/partnerapi/deletecustomorg')
+        .with(
+          headers: { 'Host' => 'my.partnerdomain.com', 'Authorization' => 'Bearer tok3n' },
+          body: "OrganizationID=#{id}"
+        )
+        .to_return(status: 200, body: organization, headers: {})
+
+      organization = RallyUp::Partner::Organization.delete(id)
+      expect(organization.id).to eq(12_345)
+    end
+  end
+
   describe '#list' do
     it 'makes HTTP GET request and renders Organizations' do
       stub_request(:get, 'https://my.partnerdomain.com/v1/partnerapi/organizations')
